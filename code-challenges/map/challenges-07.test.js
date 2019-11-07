d2ede55ffdb94e5f24cdd0bd911c525884add43f
Terrell Douglas <terrelloydouglas@yahoo.com>
2019-11-06 23:00:06 -0800

    I think Im all caught back up with challenges

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -12,7 +12,7 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   let arr2 = [];
-  for(let value of arr){
+  for (let value of arr) {
     arr2.push(Math.pow(2, value));
   }
   return arr2;
@@ -68,7 +68,7 @@ const evenOdd = (arr) => {
   return arr.map(value => {
     if (isNaN(value)) {
       return 'N/A';
-    }else if (value % 2 === 0) {
+    } else if (value % 2 === 0) {
       return 'even';
     } else {
       return 'odd';
@@ -167,7 +167,8 @@ const snorlaxStats = {
 
 const extractStats = (arr) => {
   return arr.map(ability => {
-
+    return {name: ability.stat.name, total: ability.baseStat + ability.effort, };
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -216,29 +217,29 @@ describe('Testing challenge 3', () => {
 
 describe('Testing challenge 4', () => {
   test('It should return an array containing the character code for each letter', () => {
-    expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([ 67, 111, 100, 101, 51, 48, 49 ]);
+    expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1'])).toStrictEqual([67, 111, 100, 101, 51, 48, 49]);
     expect(charCode(['C', 'o', 'd', 'e', '3', '0', '1']).length).toStrictEqual(7);
   });
 });
 
 describe('Testing challenge 5', () => {
   test('It should return an array containing the keys from an object', () => {
-    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
+    expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual(['odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd']);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
   });
 
   test('It should work with all odd numbers', () => {
-    expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual([ 'odd', 'odd', 'odd', 'odd', 'odd' ]);
+    expect(evenOdd([1, 3, 5, 7, 9])).toStrictEqual(['odd', 'odd', 'odd', 'odd', 'odd']);
     expect(evenOdd([1, 3, 5, 7, 9]).length).toStrictEqual(5);
   });
 
   test('It should work with all even numbers', () => {
-    expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual([ 'even', 'even', 'even', 'even', 'even' ]);
+    expect(evenOdd([2, 4, 6, 8, 10])).toStrictEqual(['even', 'even', 'even', 'even', 'even']);
     expect(evenOdd([2, 4, 6, 8, 10]).length).toStrictEqual(5);
   });
 
   test('It should return the string "N/A" if a non-number is included in the array', () => {
-    expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual([ 'odd', 'even', 'even', 'N/A' ]);
+    expect(evenOdd([5, 8, 2, 'hi'])).toStrictEqual(['odd', 'even', 'even', 'N/A']);
     expect(evenOdd([5, 8, 2, 'hi']).length).toStrictEqual(4);
   });
 });
@@ -260,3 +261,4 @@ xdescribe('Testing challenge 7', () => {
     expect(extractStats(snorlaxStats.stats).length).toStrictEqual(3);
   });
 });
+
